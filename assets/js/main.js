3f645e8153a20079ecbb9b2a0abb7b3c166caff2
the-zer0 <manomannem@gmail.com>
2024-11-06 17:36:30 +0530

    Updated the code of email.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -208,3 +208,17 @@ function sendEmail(event) {
             document.querySelector('.error-message').textContent = "There was an error sending your message. Please try again.";
         });
 }
+
+
+function sendEmail(){
+  var params = {
+    name : document.getElementById("name").value,
+    email : document.getElementById("email").value,
+    subject : document.getElementById("subject").value,
+    message : document.getElementById("message").value,
+  }
+
+  emailjs.send("service_hyley4e","template_lktf7ef",params).then(function (res){
+    alert("Success");
+  })
+}
